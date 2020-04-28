48a45c180db135c98746ac4f10b2f7af5eac382e
Alexandre Buhler <al.buhler@gmail.com>
2020-04-28 18:22:41 -0300

    Projeto iniciado

---
+++ Secao07/Secao07/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace Secao07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeSpan ts = TimeSpan.FromMilliseconds(0.5);
+            Console.WriteLine(ts);
+
+            DateTime d1 = DateTime.Now;
+            Console.WriteLine(d1.ToLongDateString().ToString(CultureInfo.InvariantCulture) + ", " +  d1.Kind);
+        }
+    }
+}
